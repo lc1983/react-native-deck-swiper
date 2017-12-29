0c803c75cfb57c0f0916f73968bd024a12390c4d
LONG CHENG <chenglon@gmail.com>
2017-12-29 09:28:29 -0800

    Point Swiper dependency to local for the example project.

--- Exemples/App.js
+++ Exemples/App.js
@@ -6,7 +6,7 @@ export default class Exemple extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      cards: ['1', '2', '3'],
+      cards: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
       swipedAllCards: false,
       swipeDirection: '',
       isSwipingBack: false,
